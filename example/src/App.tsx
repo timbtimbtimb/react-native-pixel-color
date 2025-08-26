29af86eb0b5841bf1deddd9834020c919ed888f6
Timothee Billiet Cadart <billiet.timothee@gmail.com>
2025-08-26 14:08:23 +0200

    test: updated example to show sampled color

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -3,19 +3,19 @@ import { Text, View, StyleSheet, Image } from 'react-native';
 import { getPixelColor } from '@snowmap.fr/react-native-pixel-color';
 
 export default function App() {
-  const [color, setColor] = useState('');
+  const [color, setColor] = useState({ red: 0, green: 0, blue: 0, alpha: 0 });
   const [loading, setLoading] = useState(true);
 
-  const x = 10;
-  const y = 10;
+  const x = 5;
+  const y = 5;
   const base64 =
     'iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAAApgAAAKYB3X3/OAAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAANCSURBVEiJtZZPbBtFFMZ/M7ubXdtdb1xSFyeilBapySVU8h8OoFaooFSqiihIVIpQBKci6KEg9Q6H9kovIHoCIVQJJCKE1ENFjnAgcaSGC6rEnxBwA04Tx43t2FnvDAfjkNibxgHxnWb2e/u992bee7tCa00YFsffekFY+nUzFtjW0LrvjRXrCDIAaPLlW0nHL0SsZtVoaF98mLrx3pdhOqLtYPHChahZcYYO7KvPFxvRl5XPp1sN3adWiD1ZAqD6XYK1b/dvE5IWryTt2udLFedwc1+9kLp+vbbpoDh+6TklxBeAi9TL0taeWpdmZzQDry0AcO+jQ12RyohqqoYoo8RDwJrU+qXkjWtfi8Xxt58BdQuwQs9qC/afLwCw8tnQbqYAPsgxE1S6F3EAIXux2oQFKm0ihMsOF71dHYx+f3NND68ghCu1YIoePPQN1pGRABkJ6Bus96CutRZMydTl+TvuiRW1m3n0eDl0vRPcEysqdXn+jsQPsrHMquGeXEaY4Yk4wxWcY5V/9scqOMOVUFthatyTy8QyqwZ+kDURKoMWxNKr2EeqVKcTNOajqKoBgOE28U4tdQl5p5bwCw7BWquaZSzAPlwjlithJtp3pTImSqQRrb2Z8PHGigD4RZuNX6JYj6wj7O4TFLbCO/Mn/m8R+h6rYSUb3ekokRY6f/YukArN979jcW+V/S8g0eT/N3VN3kTqWbQ428m9/8k0P/1aIhF36PccEl6EhOcAUCrXKZXXWS3XKd2vc/TRBG9O5ELC17MmWubD2nKhUKZa26Ba2+D3P+4/MNCFwg59oWVeYhkzgN/JDR8deKBoD7Y+ljEjGZ0sosXVTvbc6RHirr2reNy1OXd6pJsQ+gqjk8VWFYmHrwBzW/n+uMPFiRwHB2I7ih8ciHFxIkd/3Omk5tCDV1t+2nNu5sxxpDFNx+huNhVT3/zMDz8usXC3ddaHBj1GHj/As08fwTS7Kt1HBTmyN29vdwAw+/wbwLVOJ3uAD1wi/dUH7Qei66PfyuRj4Ik9is+hglfbkbfR3cnZm7chlUWLdwmprtCohX4HUtlOcQjLYCu+fzGJH2QRKvP3UNz8bWk1qMxjGTOMThZ3kvgLI5AzFfo379UAAAAASUVORK5CYII=';
 
   useEffect(() => {
+    setLoading(true);
     getPixelColor(base64, x, y)
       .then((c) => {
-        console.log('Pixel color:', c);
-        setColor(JSON.stringify(c));
+        setColor(c);
       })
       .catch(console.error)
       .finally(() => {
@@ -25,14 +25,27 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>Result: {color}</Text>
-      <Text>Loading: {JSON.stringify(loading)}</Text>
-      <Text>x: {x}</Text>
-      <Text>y: {y}</Text>
       <Image
         source={{ uri: `data:image/png;base64,${base64}` }}
         style={styles.image}
       />
+      <View>
+        <Text>x: {x}</Text>
+        <Text>y: {y}</Text>
+      </View>
+      <Text>Loading: {JSON.stringify(loading)}</Text>
+      <View
+        style={{
+          ...styles.color,
+          backgroundColor: `rgba(${color.red}, ${color.green}, ${color.blue}, ${color.alpha})`,
+        }}
+      />
+      <View>
+        <Text>r: {color.red}</Text>
+        <Text>g: {color.green}</Text>
+        <Text>b: {color.blue}</Text>
+        <Text>a: {color.alpha}</Text>
+      </View>
     </View>
   );
 }
@@ -42,10 +55,16 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    gap: 50,
   },
   image: {
     width: 100,
     height: 100,
-    marginTop: 50,
+  },
+  color: {
+    width: 100,
+    height: 100,
+    borderWidth: 1,
+    borderColor: 'black',
   },
 });
